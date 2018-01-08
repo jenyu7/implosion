3a6fd5559baf3792de5c3de0133ac2cb6724162c
adeeburrahman <57969730+adeeburrahman@users.noreply.github.com>
2018-01-07 23:56:42 -0500

    fix deck creation

--- fxn.c
+++ fxn.c
@@ -1,80 +1,56 @@
 #include "header.h"
 
 // Deck
-// 38 + exploding = total_cards
-// exploding_kittens = players - 1
-// 1 DEFUSE in deck
-// 5 NOPE
 // 4 ATTACK
 // 4 SKIP
 // 4 FAVOR
 // 4 SHUFFLE
-// 4 SEE THE FUTURE
-// 4 TACOCAT, 4 RAINBOW-RALPHING CAT, 4 HAIRY POTATO CAT
+// 5 SEE THE FUTURE
+// 4 TACOCAT
+// 4 RAINBOW-RALPHING CAT
+// 4 HAIRY POTATO CAT
+// 5 NOPE
+// 6 - players DEFUSE
+// players - 1 EXPLODNG KITTENS
 
-char ** create_deck(int players) {
-  printf("Creating deck...\n");
-  int i, pos, exploding_kittens, total_cards;
-  pos = 0;
-  exploding_kittens = players - 1;
-  total_cards = 38 + exploding_kittens;
- 
-  char ** deck = calloc(total_cards, sizeof(char *));
-  printf("after calloc\n");
-  
-  for (i = 0; i < 4; i++) {
-    printf("Pre-attack\n");
-    printf("pos: %d\n", pos);
-    // Attack
-    strcpy(deck[pos], "Attack"); // SEGFAULT
-    pos++;
-    printf("pos: %d\n", pos);
-    // Skip
-    strcpy(deck[pos], "Skip");
-    pos++;
-    // Favor
-    strcpy(deck[pos], "Favor");
-    pos++;
-    // Shuffle
-    strcpy(deck[pos], "Shuffle");
-    pos++;
-    // See The Future
-    strcpy(deck[pos], "See The Future");
-    pos++;
-    // Tacocat
-    strcpy(deck[pos], "Tacocat");
-    pos++;
-    // Rainbow-ralphing Cat
-    strcpy(deck[pos], "Rainbow-ralphing Cat");
-    pos++;
-    // Hairy Potato Cat
-    strcpy(deck[pos], "Hairy Potato Cat");
-    pos++;
-  }
-  
-  for (i = 0; i < 5; i++) {
-    // Nope
-    strcpy(deck[pos], "Nope");
-    pos++;
-  }
+int add_cards(char ** deck, char * card_name, int amount, int pos){
+  for(;amount-->0;) deck[pos++] = card_name;
+  return pos;
+}
 
-  for (i = 0; i < exploding_kittens; i++) {
-    // Exploding Kitten
-    strcpy(deck[pos], "Exploding Kitten");
-    pos++;
-  }
+char ** create_deck(int players, int * deck_size) {
+  int pos = 0;
+  *deck_size = 38 + (6 - players) + (players - 1);
+  char ** deck = calloc(*deck_size, sizeof(char *));
 
-  // Defuse
-  strcpy(deck[pos], "Defuse");
-  
+  pos = add_cards(deck, "Attack", 4, pos);
+  pos = add_cards(deck, "Skip", 4, pos);
+  pos = add_cards(deck, "Favor", 4, pos);
+  pos = add_cards(deck, "Shuffle", 4, pos);
+  pos = add_cards(deck, "See The Future", 5, pos);
+  pos = add_cards(deck, "Tacocat", 4, pos);
+  pos = add_cards(deck, "Rainbow-ralphing Cat", 4, pos);
+  pos = add_cards(deck, "Hairy Potato Cat", 4, pos);
+  pos = add_cards(deck, "Nope", 5, pos);
+  pos = add_cards(deck, "Defuse", 6 - players, pos);
+  pos = add_cards(deck, "Exploding Kitten", players - 1, pos);
+
   return deck;
+}
+
+void print_deck(char ** deck, int size) {
+  int i;
+  for(i = 0; i < size; i++) {
+      printf("%s\n", deck[i]);
+  }
 }
 
 int main() {
 
-  printf("main...\n");
-  create_deck(2);
-
+  int size;
+  char ** deck = create_deck(2, &size);
+  print_deck(deck, size);
+  free(deck);
   return 0;
 
 }
