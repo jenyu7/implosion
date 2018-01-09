0f843310b509a33036febef300ee94fbc45be91f
Marcus Ng <marcuslistanng@gmail.com>
2018-01-08 20:08:03 -0500

    added shuffle and see the future

--- fxn.c
+++ fxn.c
@@ -32,7 +32,7 @@ char ** create_deck(int players, int * deck_size) {
   pos = add_cards(deck, "Rainbow-ralphing Cat", 4, pos);
   pos = add_cards(deck, "Hairy Potato Cat", 4, pos);
   pos = add_cards(deck, "Catermelon", 4, pos);
-  pos = add_cards(deck, "Bearded Cat", 4, pos);
+  pos = add_cards(deck, "Beard Cat", 4, pos);
   pos = add_cards(deck, "Nope", 5, pos);
   pos = add_cards(deck, "Defuse", 6 - players, pos);
   pos = add_cards(deck, "Exploding Kitten", players - 1, pos);
@@ -42,14 +42,48 @@ char ** create_deck(int players, int * deck_size) {
 
 void print_deck(char ** deck, int size) {
   int i;
-  for(i = 0; i < size; i++) printf("%s\n", deck[i]);
+  for(i = 0; i < size; i++) {
+    printf("%s\n", deck[i]);
+  }
 }
+
+void swap(char * a, char * b) {
+  char tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void shuffle(char ** deck, int size) {
+  srand(time(NULL));
+  int r = rand();
+  int i, j;
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < size; j++) {
+      int pos = rand() % size;
+      swap(&deck[i], &deck[pos]);
+    }
+  }
+}
+
+void see_the_future(char ** deck) {
+  int i;
+  for (i = 0; i < 3; i++) {
+    printf("Card %d: %s\n", i + 1, deck[i]);
+  }
+}
+
 
 int main() {
 
   int size;
   char ** deck = create_deck(2, &size);
   print_deck(deck, size);
+  
+  shuffle(deck, size);
+  
+  see_the_future(deck);
+  print_deck(deck, size);
+  
   free(deck);
   return 0;
 
